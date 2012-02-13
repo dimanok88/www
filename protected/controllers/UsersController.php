37c28e1574ca9b9d43efd8a1e2bc1c137ce45bcd
Dima <nike@dima.(none)>
2012-02-13 22:23:21 +0400

    zend dom element

--- protected/controllers/UsersController.php
+++ protected/controllers/UsersController.php
@@ -131,41 +131,70 @@ class UsersController extends Controller {
     {
         Yii::import('application.vendors.*');
         require_once('Zend/Dom/Query.php');
+
+        $page= array();
         
         $obj = CurlAuth::init() ->login('http://www.autoshinavrn.ru/index.php')->load('http://www.autoshinavrn.ru/setuser.php?user=romachu');
         $result =  $obj->load('http://www.autoshinavrn.ru/viewpage.php?page_id=1&parms=1002000201')->content;
         //echo $result;
 
         $dom = new Zend_Dom_Query($result);
-        
-        $results = $dom->query('//table.tbl-border/tr');
-        
-        $i = 0;
-        $ar = array();
-        foreach ($results as $res) {
-            if(($i != 0 && $i != 1) && $i != count($results)-1 && $i != count($results)-2){
-                $k = 0;
-               foreach($res->childNodes as $ch)
-               {
-                   if($k == 0 || $k == 2 || $k == 4){
-                       $string = rtrim(preg_replace('/\\s+$/i','', $ch->nodeValue));
-                       if($k == 0){
-                           $s = trim(mb_convert_encoding(preg_replace('/\(Остаток.*$/i','',str_replace(" «фото»", "", $string)), 'windows-1251', 'utf-8'));
-                           preg_match_all("!(.*)\s+(.*?)$!i", $s, $r);
-                           print_r($r);
-                           //$ar[$i][$k] = $r;
-                       }
-                       else $ar[$i][$k] = $string;
-                   }
-                   $k++;
-               }
-            }
-            $i++;
+        $result_page = $dom->query('//table.spacer//th//a.side');
+
+        foreach($result_page as $p)
+        {
+           if(is_numeric($p->nodeValue))
+               $page[] = $p->nodeValue;
         }
 
+        $i = 0;
+        foreach($page as $p_n){
+            $result =  $obj->load('http://www.autoshinavrn.ru/viewpage.php?page_id=1&parms=100200020'.$p_n)->content;
+            $dom = new Zend_Dom_Query($result);
+            $results = $dom->query('//table.tbl-border/tr');
 
+            $i = 0;
+            $ar = array();
+            $mas2 = array();
+            foreach ($results as $res) {
+                if(($i != 0 && $i != 1) && $i != count($results)-1 && $i != count($results)-2){
+                    $k = 0;
+                   foreach($res->childNodes as $ch)
+                   {
+                       if($k == 0 || $k == 2 || $k == 4){
+                           $string = rtrim(preg_replace('/\\s+$/i','', $ch->nodeValue));
+                           if($k == 0){
+                               $s = trim(preg_replace('/\(Остаток.*$/i','',str_replace(" «фото»", "", $string)));
+                               //preg_match_all("!(.*)\s+(.*?)$!i", $s, $r);
+                               //print_r($r);
+                               $t = preg_replace('!\pZ+$!i', '', urlencode($s));
+                               $t = str_replace('%C2%A0', '',$t);
+                               $t = urldecode($t);
+                               //$t = iconv('UTF-8','WINDOWS-1251', $t);
+                               $t =  trim(mb_convert_encoding($t, 'koi8-r', 'utf-8'));
+                               preg_match_all("!(.*)\s+(.*?)$!i", $t, $r);
+                               $ar[$i][$k] = $r[1][0];
+                               $ar[$i]['country'] = $r[2][0];
+                           }
+                           else $ar[$i][$k] = trim($string);
+                       }
+                       $k++;
+                   }
+                }
+                $i++;
+                $mas2  = array_merge($ar
+                //echo $i."<br/>";
+            }
+            
 
-        CVarDumper::dump($ar,10,true);
+            /*foreach($ar as $its)
+            {
+
+            }*/
+            if($p_n >3) break;
+        }
+        CVarDumper::dump($mas2, 10,true);
+
     }
 
 
